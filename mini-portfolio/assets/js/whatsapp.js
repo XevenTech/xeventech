31ca8000642afd964e42b719d722e85db7022d21
Xeven Tech <xeventech.pk@gmail.com>
2025-02-07 16:59:48 +0500

    Delete mini-portfolio directory

--- mini-portfolio/assets/js/whatsapp.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function() {
-    // This can be used to add any interactive behavior on scroll
-    let button = document.querySelector('.whatsapp-button img');
-    button.style.transform = 'scale(1.2)';
-    
-    setTimeout(function() {
-        button.style.transform = 'scale(1)';
-    }, 300);
-});
